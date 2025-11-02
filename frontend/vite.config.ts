66997f8b250687195fa7430a23fc443a57c0a594
FFANG399 <fangxiny399@gmail.com>
2025-11-03 04:04:21 +0800

    Fix deployment issues

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  /*
   server: {
     proxy: {
       '/api': {
@@ -18,5 +17,5 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/static/, '/static')
       }
     }
-  }*/
+  }
 })
