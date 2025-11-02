5217d373e400fc76e3b2fa69b5816fa28ba73afc
YUEWEI ZHANG Z5502110 <1367949528@qq.com>
2025-11-02 21:41:59 +0800

    feat(backend): add backend folder

---
+++ backend/alembic/versions/04622b31c520_.py
@@ -0,0 +1,17 @@
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '04622b31c520'
+down_revision = ('2b3c4d5e6f7g', '5e6f7a8b9c0d')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
